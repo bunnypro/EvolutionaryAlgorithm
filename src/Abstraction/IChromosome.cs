78889675361bd50e8c8cbdc0860424a822a859ea
Mubarrid Albar <mubarrid.albar@outlook.com>
2019-11-22 11:21:24 +0700

    objective values should be comparable

--- src/Abstraction/IChromosome.cs
+++ src/Abstraction/IChromosome.cs
@@ -3,7 +3,6 @@ using System;
 namespace EvolutionaryAlgorithm.Abstraction
 {
     public interface IChromosome<T> : IEquatable<IChromosome<T>>
-        where T : IComparable<T>
     {
         T Fitness { get; }
     }
